095c055b81e4be1c1908694132d18191e9fdec65
subhramukherjee2398 <subhramukherjee560@gmail.com>
2022-04-27 05:22:38 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -5,17 +5,38 @@ import("./Math").then((result) => {
 });
 
 const Home = React.lazy(() => import("./Home"));
-const About = React.lazy(()=>import('./About'))
+const About = React.lazy(() => import("./About"));
 function App() {
   return (
     <div className="App" style={{ display: "flex", flexDirection: "row" }}>
-      <Suspense fallback={<div><img src="https://wpamelia.com/wp-content/uploads/2018/11/ezgif-2-6d0b072c3d3f.gif" alt="homeloader" width="180px" height="100px" /></div>}>
+      <Suspense
+        fallback={
+          <div>
+            <img
+              src="https://wpamelia.com/wp-content/uploads/2018/11/ezgif-2-6d0b072c3d3f.gif"
+              alt="homeloader"
+              width="180px"
+              height="100px"
+            />
+          </div>
+        }
+      >
         <Home />
       </Suspense>
-      <Suspense fallback={<div><img src="https://icon-library.com/images/gif-icon/gif-icon-0.jpg" alt="aboutLoader" width="180px" height="100px" /></div>}>
-      <About />
+      <Suspense
+        fallback={
+          <div>
+            <img
+              src="https://icon-library.com/images/gif-icon/gif-icon-0.jpg"
+              alt="aboutLoader"
+              width="180px"
+              height="100px"
+            />
+          </div>
+        }
+      >
+        <About />
       </Suspense>
-     
     </div>
   );
 }
