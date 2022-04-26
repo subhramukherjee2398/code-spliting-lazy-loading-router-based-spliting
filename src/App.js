d7af04e6a58f5b5eb34f4229025c346480d8af68
subhramukherjee2398 <subhramukherjee560@gmail.com>
2022-04-27 04:40:05 +0530

    add lazy loading for the components.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import React, { Suspense } from "react";
+import "./App.css";
+import("./Math").then((result) => {
+  console.warn("result = ", result.Add(100, 200));
+});
 
-import About from './About';
-import './App.css';
-import Home from './Home';
-import { Add } from './Math';
-console.warn(Add(5,10))
-
+const Home = React.lazy(() => import("./Home"));
+const About = React.lazy(()=>import('./About'))
 function App() {
   return (
-    <div className="App" style={{display:"flex",flexDirection:'row'}}>
-      <Home/>
-      <About/>
+    <div className="App" style={{ display: "flex", flexDirection: "row" }}>
+      <Suspense fallback={<div><img src="https://wpamelia.com/wp-content/uploads/2018/11/ezgif-2-6d0b072c3d3f.gif" alt="homeloader" width="180px" height="100px" /></div>}>
+        <Home />
+      </Suspense>
+      <Suspense fallback={<div><img src="https://icon-library.com/images/gif-icon/gif-icon-0.jpg" alt="aboutLoader" width="180px" height="100px" /></div>}>
+      <About />
+      </Suspense>
+     
     </div>
   );
 }
